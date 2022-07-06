2aac0f63d3342e7d831ca91c27d35199654db8fc
ichengi <39175857+ichengi@users.noreply.github.com>
2022-07-06 12:24:12 +0800

    Create get_wechat_gzh.py
    利用selenium模拟登录微信开发者平台，通过获取超链接-其他公众号文章的方式得到token和请求地址，再通过selenium保存session信息模拟请求，获取文章json数据，处理并存入数据库（pg)

---
+++ wx/get_wechat_gzh.py
@@ -0,0 +1,16 @@
+#获取微信公众号文章列表   包含：标题、简介、地址、封面图
+#需求  BeautifulSoup #解析网页   selenium  # 模拟登录   psycopg # 存储数据 
+#chromedriver_win32  # 谷歌浏览器驱动，selenium需要
+def get_gzh(save_dir:str)->None:
+  pass
+
+def save_data_into_pgsql(table_name:str , fields:str, values:list)->None:
+  pass
+
+def json_cal(json_dir:str)->None:
+  pass
+
+
+
+if __name__=='__main__':
+  pass
