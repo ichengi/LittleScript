2c596d16233d7078bea65e6419685d2bfd1e5c75
chengi i <1078994941@qq.com>
2022-11-22 14:55:38 +0800

    fix ： 修改存数据库

--- database/存数据.py
+++ database/存数据.py
@@ -3,30 +3,45 @@ import json
 import pymysql
 import time
 
-if __name__ == '__main__':
-    # 连接数据库
-    conn = pymysql.connect(host='localhost', user="root", password="123456" , database="test", charset="utf8")
+# 存数据库
+def insert_into_postgres(table: str, fields: str, values: list) -> None:
+    '''
+    :param table: 表名
+    :param fields:  字段名
+    :param values:  数据列表，每一项是一条记录的列表
+    :return: None
+    '''
+    couunt = len(str(fields).split(','))
+    fields = ",".join(str(fields).split(','))
+    placeholder = ",".join(['%s'] * couunt)
+    conn = psycopg2.connect(database="test", user="postgres", password="123456",
+                            host="localhost", port="5432")
     cursor = conn.cursor()
-
-    # 插入sql语句
-    sql = "insert into table123 (deviceName,value123,times) values (%s,%s,%s)"
-    lists = []
-    for list in lists:
-        deviceName = str(list[0])
-        value = str(list[1])
-        times = list[2]
-
-        # 执行插入操作
-        insert = cursor.execute(sql, (deviceName, value, times))
-
-    rs = cursor.fetchall()
-    for r in rs:
-        print(r)
-
-
-    cursor.close()
+    sql = "insert into " + table + "(" + fields + ")" + " values(" + placeholder + ")"
+    cursor.executemany(sql, values)
     conn.commit()
     conn.close()
+
+# 修改表注释
+def create_table_comment():
+    # 格式： 列1（字段名）   列二（注释）
+    # 读取数据
+    data = pd.read_excel(r'已解密.xlsx', header=0,sheet_name="数据字典")
+    conn = psycopg2.connect(database="test", user="postgres", password="123456",
+                            host="localhost", port="5432")
+    cursor = conn.cursor()
+    table_name = "sample"  # 表名
+    for idx, row in data.iterrows():
+        row = list(row)
+        field = row[0]
+        comment = row[1]
+        sql = f"""COMMENT  ON COLUMN  {table_name}.{field} IS '{comment}'"""
+        cursor.execute(sql)
+        conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    pass
 
 
 
