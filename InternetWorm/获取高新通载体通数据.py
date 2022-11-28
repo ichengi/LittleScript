98030c9f44db5f26e2d2e0d35a3c022efff16333
ichengi <1078994941@qq.com>
2022-11-28 13:48:20 +0800

    feat: 新增获取获取载体通数据

---
+++ InternetWorm/获取高新通载体通数据.py
@@ -0,0 +1,30 @@
+import json
+from time import sleep
+
+import requests
+
+# 获取载体通数据
+def get_data():
+    '''
+    :return: list
+    '''
+    res = []
+    url = "https://www.cdhtqyfw.cn/policydata/apartmentbase.html" # 这个地址 通过手机抓包获得
+    form_data = { "pageNumber":1,"pageSize":12}
+    req = requests.post(url, data=form_data)
+    total = 1
+    if req.status_code == 200:
+        data_temp = req.json()
+        total = data_temp["data"]['totalPage']
+    for i in range(total):
+        sleep(0.05)
+        form_data = {"pageNumber": i + 1, "pageSize": 12}
+        req = requests.post(url, data=form_data)
+        if req.status_code == 200:
+            data_temp = req.json()
+            res.extend(data_temp["data"]['list'])
+    with open(rf'data/高新通载体通数据.json', 'w', encoding='utf-8') as f:
+        json.dump(res, f, ensure_ascii=False, indent=4)
+
+if __name__ == '__main__':
+    get_data()
